b7c79b721d59ee56eb3df646b8509db1f712b823
Uvacoder <uva22940coder@gmail.com>
2022-10-04 20:27:12 -0400

    Update Navbar.tsx

--- src/containers/Navbar.tsx
+++ src/containers/Navbar.tsx
@@ -6,7 +6,7 @@ import ThemeToggler from '../components/Layout/ThemeToggler';
 const Navbar: React.FC = () => {
   return (
     <div className="navbar">
-      <a href="https://aykutsarac.github.io/github-rater" className="logo">
+      <a href="https://uvacoder.github.io/abc-github-rater" className="logo">
         <IoIosSpeedometer className="icon" size={28} /> GitHub Rater
       </a>
       <ThemeToggler />
